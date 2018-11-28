5547ff91d4db4b56efbe7c8d6c5fcf78359e1176
bustling-integer-5788 <sunnyksharma1@gmail.com>
2018-11-28 12:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 
 function takeANumber(line, name) {
+<<<<<<< HEAD
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`;
+=======
+  return "Welcome, " + name + ". You are number " + line.length + 1 + " in line."
+>>>>>>> 67c9655640ee427ea36a5f588d1907a552efa617
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
+<<<<<<< HEAD
     return "There is nobody waiting to be served!";
+=======
+    return "There is nobody waiting to be served!"
+>>>>>>> 67c9655640ee427ea36a5f588d1907a552efa617
   }
   
   var first = katzDeliLine[0];
   katzDeliLine.splice(0,1);
+<<<<<<< HEAD
   return "Currently serving " + first + ".";
 }
 
@@ -27,4 +36,7 @@ function currentLine(line) {
   waiting = waiting.substring(0, waiting.length - 2);
   
   return waiting;
+=======
+  return first;
+>>>>>>> 67c9655640ee427ea36a5f588d1907a552efa617
 }
